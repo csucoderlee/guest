d43e927a8fc16e3f3250930e7bd4e946f21a50db
lixiang <csucoderlee@gmail.com>
2017-07-29 23:18:44 +0800

    简单的认证登录 cookie中返回用户的名字

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # guest
 this is django demo project
+
+## CSRF
+django针对CSRF的保护措施是在生成的每个表单中放置一个自动生成的令牌，通过这个令牌判断POST请求是否来自统一个网站。
+在form表单中添加{%  csrf_token %}
+如果要想忽略掉该检查，可以在guest/setting.py文件中注释掉csrf
--- sign/views.py
+++ sign/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 def index(request):
     # return HttpResponse("Hello Django!")
     return render(request, "index.html")
+
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        if username == 'admin' and password == 'admin123':
+            # return HttpResponse('login success')
+            response = HttpResponseRedirect('/event_manage/')
+            response.set_cookie('user', username, 3600)
+            return response
+        else:
+            return render(request, 'index.html', {'error':'username or password is wrong'})
+
+def event_manage(request):
+    username = request.COOKIES.get('user','')
+    return render(request, "event_manage.html", {'user':username})
